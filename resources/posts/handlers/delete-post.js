ed8600772916a34984dfc546dd2cffbcb612b6d9
Lucas Romero <romero.lucas@gmail.com>
2020-06-17 18:03:04 -0300

    Handle an invalid mongo Id on delete a blog post

--- resources/posts/handlers/delete-post.js
+++ resources/posts/handlers/delete-post.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const PostCommands = require("../commands/post-commands");
 
 class DeletePostHandler {
@@ -8,15 +9,21 @@ class DeletePostHandler {
 
   async delete(req, res) {
     try {
+      const { postId } = req.params;
+
+      if (!mongoose.Types.ObjectId.isValid(postId)) {
+        return res.status(400).send({ message: "The postId is not valid" });
+      }
+
       const command = new PostCommands({
         logger: this.logger,
         db: this.db
       });
-      const result = await command.delete(req.params.postId);
+      const result = await command.delete(postId);
       return res.status(200).send(result);
     } catch (e) {
       this.logger("delete-post.js::error", e);
-      return res.sendStatus(500);
+      return res.status(500).send({ message: "Please contact support" });
     }
   }
 }
--- resources/posts/tests/handlers/delete-post_tests.js
+++ resources/posts/tests/handlers/delete-post_tests.js
@@ -23,7 +23,7 @@ describe("DELETE /posts", function () {
       .delete(`/posts/${testPostId}`)
       .set("Accept", "application/json")
       .expect(200)
-      .end(async (err, response) => {
+      .end((err, response) => {
         if (err) {
           done(err);
           return;
@@ -37,11 +37,12 @@ describe("DELETE /posts", function () {
 
   it("should have deletedCount equals to 0 as the postId does not exist", function (done) {
     const notFoundId = mongoose.Types.ObjectId().toString();
+
     request(app)
       .delete(`/posts/${notFoundId}`)
       .set("Accept", "application/json")
       .expect(200)
-      .end(async (err, response) => {
+      .end((err, response) => {
         if (err) {
           done(err);
           return;
@@ -49,6 +50,21 @@ describe("DELETE /posts", function () {
         expect(response.body.n).to.be.eql(0);
         expect(response.body.ok).to.be.eql(1);
         expect(response.body.deletedCount).to.be.eql(0);
+        done();
+      });
+  });
+
+  it("should return 400 if the id is not a valid mongo Id", function (done) {
+    request(app)
+      .delete("/posts/1")
+      .set("Accept", "application/json")
+      .expect(400)
+      .end((err, response) => {
+        if (err) {
+          done(err);
+          return;
+        }
+        expect(response.body.message).to.be.eql("The postId is not valid");
         done();
       });
   });
